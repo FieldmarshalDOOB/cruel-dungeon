3c2dda16eb217c66489fe6cb31de26bff68af2b0
Vadim <raspi1997@gmail.com>
2025-01-18 23:39:34 +0300

    Разбираюсь с краями мира

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,1 +1,12 @@
 extends CharacterBody2D
+
+const SPEED = 200
+
+#"move_left"	or	LEFT	= -1,0
+#"move_right"	or	RIGHT	= 1,0
+#"move_up"		or	UP		= 0,1
+#"move_down"	or	DOWN 	= 0=-1
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity = direction * SPEED
+	move_and_slide()
