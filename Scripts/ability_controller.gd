9e92849eaf3c353b895743f27ac3e0bbb5f39562
Vadim <raspi1997@gmail.com>
2025-01-25 11:25:34 +0300

    Настроил спавн скелетов по клику

---
+++ Scripts/ability_controller.gd
@@ -0,0 +1,26 @@
+extends Node
+
+@export var spawn_ability: PackedScene
+
+@onready var player = get_tree().get_first_node_in_group("player") as Node2D
+
+var spawn_point:Vector2
+var cooldown:bool
+
+func _process(_delta):
+	pass
+
+
+func _input(_event):
+	if Input.is_action_just_pressed("right_click") == true and cooldown == false:
+		spawn_point = player.global_position 
+		if player == null:
+			return
+		var ability_instance = spawn_ability.instantiate() as Node2D
+		player.get_parent().add_child(ability_instance)
+		ability_instance.global_position = player.global_position
+		cooldown = true
+		$Cooldown.start()
+
+func _on_timer_timeout():
+	cooldown = false
