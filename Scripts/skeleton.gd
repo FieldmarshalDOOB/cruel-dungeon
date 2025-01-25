cc40e2e41480351be24bb0639c9b77e636688817
Vadim <raspi1997@gmail.com>
2025-01-25 08:28:18 +0300

    Обновил скрипт скелета на skeleton.gd, старый скрипт оставил в skeleton_old.gd

--- Scripts/skeleton.gd
+++ Scripts/skeleton.gd
@@ -3,14 +3,10 @@ extends CharacterBody2D
 
 var speed = 70 #Чем выше тем медленее - мы делем на скокрость
 var player_chaise = false
-var player = null
 var hp = 100
-var player_in_attack_zone = false
-var can_take_dmg = true
-var hp_regeneration = 0
+var player = get_tree().get_first_node_in_group("player") as Node2D
 
 func _physics_process(_delta: float) -> void:
-	deal_with_damage()
 	update_health()
 	
 	if player_chaise:
@@ -24,59 +20,31 @@ func _physics_process(_delta: float) -> void:
 		skeleton_sprite.play("idle")
 
 
-func _on_detection_area_body_entered(body: Node2D) -> void:
+func update_health():
+	var hp_bar = $skeleton_hp_bar
+	hp_bar.value = hp
+	#Прячет полоску хп если хп фулл
+	if hp >= 100:
+		hp_bar.visible = false
+	else:
+		hp_bar.visible = true
+
+
+func _on_detection_area_body_entered(body: Node2D):
 	player = body
 	player_chaise = true
 
 
-func _on_detection_area_body_exited(_body: Node2D) -> void:
+func _on_detection_area_body_exited(_body: Node2D):
 	player = null
 	player_chaise = false
 
-func enemy():
-	pass
+
+func _on_enemy_hitbox_body_entered(body: Node2D):
+	if body == player:
+		pass
 
 
-func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
-	if body.has_method("player"):
-		player_in_attack_zone = true
-
-
-func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
-	if body.has_method("player"):
-		player_in_attack_zone = false
-
-func deal_with_damage():
-	if player_in_attack_zone and global.player_current_attack == true:
-		if can_take_dmg:
-			hp -= 20
-			$AnimatedSprite2D.play("hit")
-			$take_damage_cooldown.start()
-			can_take_dmg = false
-			#print("Skeleton HP: " + str(hp))
-			if hp <= 0:
-				$AnimatedSprite2D.play("death")
-				self.queue_free()
-
-
-func update_health():
-	var hpbar = $skeleton_hp_bar
-	hpbar.value = hp
-	#Прячет полоску хп если хп фулл
-	if hp >= 100: #убрать если в будет худ
-		hpbar.visible = false
-	else:
-		hpbar.visible = true
-
-
-func _on_take_damage_cooldown_timeout() -> void:
-	can_take_dmg = true
-
-
-func _on_hp_regen_timeout() -> void:
-	if hp < 100:
-		hp += hp_regeneration
-		if hp > 100:
-			hp = 100
-	if hp <= 0:
-		hp = 0
+func _on_enemy_hitbox_body_exited(body: Node2D):
+	if body == player:
+		pass
